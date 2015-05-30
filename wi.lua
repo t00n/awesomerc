5c6ac6e5adae37bc3c77858abb180bc51c94a040
t00n <kelkununtel@hotmail.com>
2015-05-31 00:52:30 +0200

    Revert "[fix] for awesome-mate"
    This reverts commit 2fcc85638f59e98b913918a241b5a7dee4e555f0.

--- wi.lua
+++ wi.lua
@@ -121,11 +121,3 @@ wifiicon:set_image(beautiful.widget_wifi)
 wifi = wibox.widget.textbox()
 vicious.register(wifi, vicious.widgets.wifi, "${ssid}  Débit: ${rate}Mb/s  Réception: ${link}%", 3, "wlp3s0")
 -- End Wifi }}}
-
---Weather Widget
-weather = wibox.widget.textbox()
-vicious.register(weather, vicious.widgets.weather, "${city}:  Ciel: ${sky}  T°: ${tempc}°C  Humidité: ${humid}%  Vent: ${windkmh} km/h", 1200, "EBBR")
-
---Battery Widget
-batt = wibox.widget.textbox()
-vicious.register(batt, vicious.widgets.bat, "Batt: $2% Rem: $3", 61, "BAT")
